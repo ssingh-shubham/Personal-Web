6d73ff5794ab5028b6b843ea30213bb47b2acf72
ssingh-shubham <118057692+ssingh-shubham@users.noreply.github.com>
2024-11-10 02:17:43 +0530

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed(".typewriter-text ",
+    {
+        strings: [" ",
+            "Software Development",
+           
+            "Machine Learning Algorithmns",
+           
+            "Deep Learning Algorithmns",
+           
+            "Cloud Computing",
+           
+            "Cybersecurity"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        
+    }
+)
